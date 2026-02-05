60b1d64ed383efa96a0c062e30893b1932379aff
Manswi Ajankar <manswiajankar23@gmail.com>
2026-02-05 17:40:57 +0530

    Added all mini project pages and updated index

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMenu(){
+let sidebar=document.getElementById("sidebar");
+sidebar.style.left= sidebar.style.left=="0px" ? "-250px":"0px";
+}
+
+function showPage(page){
+document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
+document.getElementById(page).classList.add("active");
+}
+
+function openProfile(){
+document.getElementById("profile").style.display="block";
+}
+
+function closeProfile(){
+document.getElementById("profile").style.display="none";
+}
+function openLogin(){
+document.getElementById("loginSidebar").classList.add("active");
+}
+
+function closeLogin(){
+document.getElementById("loginSidebar").classList.remove("active");
+}
